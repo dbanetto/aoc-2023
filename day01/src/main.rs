6acd03d7af6d6302f3980455016928003f10165d
David Barnett <david@barnett.net.nz>
2023-12-07 18:53:07 +1100

    complete day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,26 +7,56 @@ fn main() {
     let lines = io::stdin().lines();
 
     for line in lines {
-
-        if let Ok(line) = line {
-            let digits = line
-                .chars()
-                .filter_map(|char| {
-                    char.to_digit(10)
-                });
-
-            
-            let first = digits.clone().next();
-            let last = digits.last();
-
-            let val = match (first, last) {
-                (Some(f), Some(l)) => (f * 10) + l,
-                (_, _) => continue,
-            };
-            
-            count += val;
+        if let Some(line) = line.ok() {
+            let (first, last) = translate_line(line.as_ref());
+            count += (first * 10) + last;
         }
     }
 
     println!("{:?}", count);
+}
+
+fn translate_line(line: &str) -> (i32, i32) {
+
+    let mut first: Option<(usize, i32)> = None;
+    let mut last: Option<(usize, i32)> = None;
+
+    for (word, val) in [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("1",1),
+        ("2",2),
+        ("3",3),
+        ("4",4),
+        ("5",5),
+        ("6",6),
+        ("7",7),
+        ("8",8),
+        ("9",9),
+    ] {
+        if let Some(idx) = line.find(word) {
+            first = match first {
+                None => Some((idx, val)),
+                Some((fidx, _)) if fidx >= idx => Some((idx, val)),
+                _ => first
+            };
+        }
+
+        if let Some(idx) = line.rfind(word) {
+            last = match last {
+                None => Some((idx, val)),
+                Some((lidx, _)) if lidx <= idx => Some((idx, val)),
+                _ => last
+            };
+        }
+    }
+
+    return (first.unwrap_or_default().1, last.unwrap_or_default().1);
 }
